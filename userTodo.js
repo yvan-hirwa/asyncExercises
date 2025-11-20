f9a494f02137945441b2aa77290248bf8de65377
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-11-20 17:03:22 +0200

    Finished using promise.all for fetching

--- userTodo.js
+++ userTodo.js
@@ -11,11 +11,28 @@ function fetchUserTodos(user, todo ){
         }
         return response.json()
     })
-return Promise.all([users, todos]).then(console.log).catch(error => `The following error occurred while fetching: ${error}`)
+return Promise.all([users, todos])
+        .then(([users, todos]) => {
+           const result = users.map(user =>{
+                const todo = todos.filter(t => t.userId === user.id)
+
+                return{
+                    id: user.id,
+                    name: user.name,
+                    todo: todo.map(x=> {
+                        return{ userId,
+                        completed} = x
+                    })
+
+                }
+            })
+            console.log(result)
+        })
+        .catch(error => console.log(`The following error occurred while fetching: ${error}`))
 }
 
 
-// fetchUserTodos('https://jsonplaceholder.typicode.com/users', 'https://jsonplaceholder.typicode.com/todos')
+fetchUserTodos('https://jsonplaceholder.typicode.com/users', 'https://jsonplaceholder.typicode.com/todos')
 
 
 
