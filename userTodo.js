a914bc2d8721ac755aa8b4a2d65486ab6e9e1589
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-11-21 10:20:42 +0200

    Fetch users and their todo list

--- userTodo.js
+++ userTodo.js
@@ -13,33 +13,62 @@ function fetchUserTodos(user, todo ){
     })
 return Promise.all([users, todos])
         .then(([users, todos]) => {
-           const result = users.map(user =>{
+           return users.map(user =>{
                 const todo = todos.filter(t => t.userId === user.id)
 
                 return{
                     id: user.id,
                     name: user.name,
                     todo: todo.map(x=> {
-                        return{ userId,
-                        completed} = x
+                        const {userId, completed} = x
+                        return{ userId, completed}
                     })
 
                 }
             })
-            console.log(result)
+            
         })
         .catch(error => console.log(`The following error occurred while fetching: ${error}`))
 }
 
 
 fetchUserTodos('https://jsonplaceholder.typicode.com/users', 'https://jsonplaceholder.typicode.com/todos')
+        .then(result => console.log(result))
 
 
 
 
-// async function fetchUserTodosAsync(user, todo){
-//     const users = await fetch(user)
-//     const todos = await fetch(todo)
+async function fetchUserTodosAsync(user, todo){
+    
+    try {
+        const [userRes, todoRes] = await Promise.all([fetch(user), fetch(todo)])
 
-//     return Promise.all([users, todos]).the
-// }
+        if(!userRes.ok) throw new Error('User failed to fetch')
+        if(!todoRes.ok) throw new Error('Todo failed to fetch')
+
+        const users = await userRes.json()    
+        const todos = await todoRes.json()
+        
+        return users.map(user =>{
+            const userTodos = todos.filter(t => user.id === t.userId)
+
+            return{
+                id : user.id,
+                name : user.name,
+                todo: userTodos.map(todoObj => {
+                    const {userId, completed}=todoObj
+
+                    return {userId, completed}
+                })
+            }
+        })
+        
+    } catch (error) {
+        console.log(`Error: ${error}`)
+    }
+}
+
+const res = await fetchUserTodosAsync('https://jsonplaceholder.typicode.com/users',
+                                 'https://jsonplaceholder.typicode.com/todos')
+
+console.log(res)
