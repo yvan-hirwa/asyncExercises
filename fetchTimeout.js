16db4905e325806edae16412c1bbd4eb112f1740
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-11-30 19:23:14 +0200

    Ex: Refactored the fetch with timeout

--- fetchTimeout.js
+++ fetchTimeout.js
@@ -1,14 +1,27 @@
-function fetchTimeout(url){
-    const controller = new AbortController()
-    const signal = controller.signal
-    const response = fetch(url, {signal})
-    const timer = new Promise((_, reject) => setTimeout(_ => {
-        controller.abort('Response took much time')
-        reject()
-    }, 3))
+// function fetchTimeout(url){
+//     const controller = new AbortController()
+//     const signal = controller.signal
+//     const response = fetch(url, {signal})
+//     const timer = new Promise((_, reject) => setTimeout(_ => {
+//         controller.abort('Response took much time')
+//         reject()
+//     }, 3))
 
-    return Promise.race([response, timer])
+//     return Promise.race([response, timer])
 
+// }
+
+// fetchTimeout('https://restcountries.eu/rest/v2/all').then(console.log).catch(console.error)
+
+async function fetchTimeout(url){
+    try{
+        const response = await fetch(url, {
+            signal: AbortSignal.timeout(3)
+        })
+
+        return response
+    }
+    catch(e){
+        throw e
+    } 
 }
-
-fetchTimeout('https://restcountries.eu/rest/v2/all').then(console.log).catch(console.error)
