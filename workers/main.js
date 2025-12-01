62b9d9a1a8e3b2672c3df6af7564883f36891251
yvan-hirwa <hirwakaranganwayvan@gmail.com>
2025-12-01 11:37:43 +0200

    Update: On worker

--- workers/main.js
+++ workers/main.js
@@ -1,11 +1,15 @@
 const worker = new Worker('worker.js')
 
+worker.onerror = (error) => {
+    console.error("Worker Error:", error);
+};
+
+worker.onmessage = (message) => {
+    console.log(message.data)
+    worker.terminate()
+}
+
 worker.postMessage({
     command: "filter_evens",
     numbers: [1, 2, 3, 4, 5, 6]
 })
-
-worker.onmessage = (message) => {
-    console.log(message)
-    worker.terminate()
-}
